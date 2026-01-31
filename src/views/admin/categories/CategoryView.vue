a465c9e96912fb11f164a7d528f0e89bdfcb1d21
huon-thanun <huonthanun01@gmail.com>
2026-02-01 00:45:07 +0700

    add responsive and edit in userview, edit title and description in reportview, add message success to cateforyview <= [admin]

--- src/views/admin/categories/CategoryView.vue
+++ src/views/admin/categories/CategoryView.vue
@@ -9,6 +9,8 @@ const search = ref("");
 let timeout = null;
 const isLoading = ref(false);
 const categories = ref([])
+const successMessage = ref('');
+const errorMessage = ref('');
 
 const gettoken = localStorage.getItem('token')
 
@@ -19,6 +21,13 @@ const columns = [
     { key: 'createdAt', label: 'CreatedAt' },
     // { key: 'updatedAt', label: 'UpdatedAt' }
 ];
+
+const showSuccess = (message) => {
+  successMessage.value = message;
+  setTimeout(() => {
+    successMessage.value = '';
+  }, 3000);
+};
 
 const sortBy = ref(null);
 const sortDir = ref(null);
@@ -127,7 +136,10 @@ const confirmDelete = async () => {
 
         showDeleteModal.value = false
         selectedCategoryId.value = null
+        showSuccess(`Deleted category successfully`);
     } catch (err) {
+        let errorMsg = 'Failed to delete category. Please try again.';
+        errorMessage.value = errorMsg;
         throw err
     } finally {
         isLoading.value = false
@@ -167,7 +179,10 @@ const confirmEdit = async () => {
         showEditModal.value = false;
         editName.value = "";
         editDescription.value = "";
+        showSuccess(`Updated category successfully`);
     } catch (err) {
+        let errorMsg = 'Failed to update category. Please try again.';
+        errorMessage.value = errorMsg;
         throw err
     } finally {
         isLoading.value = false;
@@ -201,9 +216,12 @@ const confirmCreate = async () => {
             showCreateModal.value = false;
             newCategoryName.value = "";
             newCategoryDescription.value = "";
+            showSuccess(`Create category successfully`);
         }
 
     } catch (err) {
+        let errorMsg = 'Failed to create category. Please try again.';
+        errorMessage.value = errorMsg;
         throw err
     } finally {
         isLoading.value = false
@@ -224,6 +242,19 @@ const openDetailModal = (category) => {
 
 <template>
     <div class="p-3">
+
+        <!-- Success Alert -->
+        <div v-if="successMessage" class="alert alert-success alert-dismissible fade show mb-4" role="alert">
+            {{ successMessage }}
+            <button type="button" class="btn-close" @click="successMessage = ''"></button>
+        </div>
+
+        <!-- Error Alert -->
+        <div v-if="errorMessage" class="alert alert-danger alert-dismissible fade show mb-4" role="alert">
+            {{ errorMessage }}
+            <button type="button" class="btn-close" @click="errorMessage = ''"></button>
+        </div>
+
         <div class="d-flex justify-content-between align-items-center mb-3">
             <div>
                 <h2 class="fw-bold">Categories</h2>
@@ -232,7 +263,7 @@ const openDetailModal = (category) => {
             <BaseButton class="d-none d-md-block" variant="primary" @click="openCreateModal" icon="plus-circle">
                 Create category
             </BaseButton>
-            <BaseButton class="d-block d-md-none" variant="primary" @click="openCreateModal" icon="plus-circle">
+            <BaseButton class="d-block d-md-none d-flex" variant="primary" @click="openCreateModal" icon="plus-circle">
                 create 
             </BaseButton>
         </div>
@@ -341,7 +372,7 @@ const openDetailModal = (category) => {
             <template #body>
                 <div v-if="selectedCategoryDetail" class="text-start">
                     <p><strong>ID:</strong> {{ selectedCategoryDetail.id }}</p>
-                    <p><strong>Name:</strong><span class="badge bg-danger ms-2 fw-normal"> {{ selectedCategoryDetail.name }}</span></p>
+                    <p><strong>Name:</strong><span class="badge bg-success ms-2 fw-normal"> {{ selectedCategoryDetail.name }}</span></p>
                     <p><strong>Description:</strong> {{ selectedCategoryDetail.description }}</p>
                     <p><strong>Created At:</strong> {{ formatDate(selectedCategoryDetail.createdAt) }}</p>
                     <p><strong>UpdatedAt At:</strong> {{ formatDate(selectedCategoryDetail.updatedAt) }}</p>
